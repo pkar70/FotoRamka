448b16a2a6e16300b3e7be312b900d1f37379c71
pkar <pkar@ieee.org>
2021-10-16 12:03:33 +0200

    feat: version 2020.08 + migracja linku

---
+++ FotoRamka/MainPage.xaml.vb
@@ -0,0 +1,200 @@
+﻿' The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+''' <summary>
+''' An empty page that can be used on its own or navigated to within a Frame.
+''' </summary>
+Partial Public NotInheritable Class MainPage
+    Inherits Page
+
+    Private _sAlbum As String = "public"
+    Private _sUrl As String = ""
+    Private moTimer As DispatcherTimer = Nothing
+    Private _iTickTime As Integer = 60
+
+    Private Async Sub uiStartStop_Click(sender As Object, e As RoutedEventArgs)
+        If moTimer IsNot Nothing Then
+            TykanieStop()
+            uiStartStop.Label = "Start"
+        Else
+            Await NextPic()
+            TykanieStart()
+            uiStartStop.Label = "Stop"
+        End If
+    End Sub
+
+    Private Sub uiDelay_Click(sender As Object, e As RoutedEventArgs)
+        Dim oMI As ToggleMenuFlyoutItem = sender
+        Dim sName As String = oMI.Name
+        If Not sName.StartsWith("uiDelay") Then Return
+
+        Try
+            _iTickTime = sName.ToLower.Replace("uidelay", "")
+        Catch ex As Exception
+            _iTickTime = 60
+        End Try
+
+        SetSettingsInt("tickTime", _iTickTime)
+        ' SetCheckMarks()
+    End Sub
+
+    Private Sub uiAlbum_Click(sender As Object, e As RoutedEventArgs)
+        Dim oMI As ToggleMenuFlyoutItem = sender
+        Dim sName As String = oMI.Name
+        If Not sName.StartsWith("uiAlbum") Then Return
+
+        _sAlbum = sName.ToLower.Replace("uialbum", "")
+
+        SetSettingsString("albumName", _sAlbum)
+        ' SetCheckMarks()
+    End Sub
+
+    Private Sub SetTitlePicName(sUrl As String)
+        If pkar.NetIsCellInet Then Return
+        Dim sTitle As String = sUrl
+        sTitle = sTitle.Replace(BaseServiceUri & "/p/store/", "")
+
+        If sTitle.ToLower.Contains("myproduction/models/") Then
+            ' http://../p/store/Public/MyProduction/Models/A/Ad/AdrianaLima/Models.001/Adriana Lima Flood166.jpg
+            ' //Public/MyProduction/Models/A/Ad/AdrianaLima/Models.001/Adriana Lima Flood166.jpg
+            sTitle = sTitle.Substring("Public/MyProduction/Models/".Length)
+            ' A/Ad/AdrianaLima/Models.001/Adriana Lima Flood166.jpg
+            If sTitle.Substring(1, 1) = "/" Then
+                sTitle = "Models//" & sTitle.Substring(5)
+            Else
+                sTitle = "Models/" & sTitle ' dla Covers, Misc, itp.
+            End If
+        End If
+
+        If sTitle.ToLower.Contains("myproduction/ero/") Then
+            sTitle = sTitle.Substring("Public/MyProduction/".Length)
+            sTitle = "//" & sTitle.Substring(5)
+        End If
+
+        'sWhereFullFoto = " ( ( path LIKE '%\FotoVideo\Analogowe\Zdjecia\%' OR path LIKE '%\FotoVideo\Cyfrowe\Zdjecia\%' ) AND path NOT LIKE '%\Ego\%' AND path NOT LIKE '%\Grlfrn\%' AND name LIKE '%jpg' ) "
+        'sWherePublic = " path LIKE '%\Public\%' "
+        'sWhereRodzina = " ( path LIKE '%\FotoVideo\Analogowe\Zdjecia\dMilek\%' OR  path LIKE '%\FotoVideo\Analogowe\Zdjecia\Tata\%' AND name LIKE '%jpg' ) "
+
+
+        ApplicationView.GetForCurrentView.Title = sTitle   ' było "FotoRamka - " &..., ale on dopisuje Fotoramka na końcu (po nazwie)
+    End Sub
+
+    Private Async Function NextPic() As Task
+
+        Dim sUrl As String = BaseServiceUri & "/p/dysk/gerrandompic.asp?album=" & _sAlbum
+
+        sUrl = Await HttpPageAsync(sUrl, "random id", False)
+        If Not sUrl.StartsWith("http") Then Return
+
+        _sUrl = sUrl
+
+        Dim oBmp As BitmapImage = New BitmapImage(New Uri(sUrl))
+        uiImage.Source = oBmp
+        SetTitlePicName(sUrl)
+    End Function
+
+
+
+    Private Sub uiPic_Tapped(sender As Object, e As RoutedEventArgs)
+        Dim oResize As Stretch = uiImage.Stretch
+        Select Case oResize
+            Case Stretch.Uniform
+                uiImage.Stretch = Stretch.None
+            Case Stretch.None
+                uiImage.Stretch = Stretch.Uniform
+        End Select
+
+    End Sub
+    Private Sub uiGetInfo_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+    Private Sub uiGetUrl_Click(sender As Object, e As RoutedEventArgs)
+        ClipPut(_sUrl)
+        DialogBox(_sUrl)
+    End Sub
+
+    Private Sub TykanieStart()
+        Try
+            moTimer = New DispatcherTimer
+            moTimer.Interval = TimeSpan.FromSeconds(_iTickTime)
+
+            AddHandler moTimer.Tick, AddressOf Timer_TickUI
+            'mTimerMode = 0
+            moTimer.Start()
+            Return
+        Catch ex As Exception
+            CrashMessageExit("@InitTykanie", ex.Message)
+
+        End Try
+    End Sub
+
+    Private Sub TykanieStop()
+        If moTimer IsNot Nothing Then
+            Try
+                RemoveHandler moTimer.Tick, AddressOf Timer_TickUI
+                moTimer.Stop()
+                moTimer = Nothing
+            Catch ex As Exception
+            End Try
+        End If
+    End Sub
+
+    Private Async Sub Timer_TickUI(sender As Object, e As Object)
+        ' If mbInDebug Then Debug.WriteLine("BasicCamera:Timer_TickUI, " & Date.Now.ToString("HH:mm:ss"))
+
+        moTimer?.Stop()
+
+        Await NextPic()
+
+        moTimer?.Start()
+
+    End Sub
+
+    Private Sub Page_Loaded(sender As Object, e As RoutedEventArgs)
+        ' wczytanie zmiennych z SetSettings i ustawienie ToggleMFI
+        _sAlbum = GetSettingsString("albumName", "public")
+        _iTickTime = GetSettingsInt("tickTime", 60)
+
+        SetCheckMarks()
+    End Sub
+
+    Private Sub SetCheckMarks()
+
+        Dim sDelay As String = _iTickTime.ToString
+        Dim bSelected As Boolean = False
+        For Each oMFI As ToggleMenuFlyoutItem In uiMenuDelay.Items
+            If oMFI.Name.EndsWith(sDelay) Then
+                oMFI.IsChecked = True
+                bSelected = True
+            Else
+                oMFI.IsChecked = False
+            End If
+        Next
+        If Not bSelected Then uiDelay60.IsChecked = True
+
+        For Each oMFI As ToggleMenuFlyoutItem In uiMenuAlbum.Items
+            If oMFI.Name.EndsWith(_sAlbum) Then
+                oMFI.IsChecked = True
+                bSelected = True
+            Else
+                oMFI.IsChecked = False
+            End If
+        Next
+        If Not bSelected Then uiAlbumPublic.IsChecked = True
+
+
+    End Sub
+
+    Private Sub Page_LosingFocus(sender As UIElement, args As LosingFocusEventArgs)
+        If Not pkar.NetIsCellInet Then Return
+        moTimer?.Stop()
+    End Sub
+
+    Private Sub Page_GotFocus(sender As Object, e As RoutedEventArgs)
+        If Not pkar.NetIsCellInet Then Return
+        moTimer?.Start()
+    End Sub
+
+    Private Sub uiGoNext_Click(sender As Object, e As RoutedEventArgs)
+        Timer_TickUI(Nothing, Nothing)
+    End Sub
+End Class
---
+++ FotoRamkaPub/FotoRamkaPub.Shared/MainPage.xaml.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace FotoRamka
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+
+        // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+
+            private string _sAlbum = "public";
+            private string _sUrl = "";
+            private DispatcherTimer moTimer = null;
+            private int _iTickTime = 60;
+
+            private async void uiStartStop_Click(object sender, RoutedEventArgs e)
+            {
+                if (moTimer is object)
+                {
+                    TykanieStop();
+                    uiStartStop.Label = "Start";
+                }
+                else
+                {
+                    await NextPic();
+                    TykanieStart();
+                    uiStartStop.Label = "Stop";
+                }
+            }
+
+            private void uiDelay_Click(object sender, RoutedEventArgs e)
+            {
+                ToggleMenuFlyoutItem oMI = (ToggleMenuFlyoutItem)sender;
+                string sName = oMI.Name;
+                if (!sName.StartsWith("uiDelay"))
+                    return;
+
+                int iValue;
+                sName = sName.ToLower().Replace("uidelay", "");
+                if(!int.TryParse(sName, out iValue))
+                    _iTickTime = 60;
+
+                p.k.SetSettingsInt("tickTime", _iTickTime);
+                // SetCheckMarks()
+            }
+
+            private void uiAlbum_Click(object sender, RoutedEventArgs e)
+            {
+                ToggleMenuFlyoutItem oMI = (ToggleMenuFlyoutItem)sender;
+                string sName = oMI.Name;
+                if (!sName.StartsWith("uiAlbum"))
+                    return;
+                _sAlbum = sName.ToLower().Replace("uialbum", "");
+            p.k.SetSettingsString("albumName", _sAlbum);
+                // SetCheckMarks()
+            }
+
+            private void SetTitlePicName(string sUrl)
+            {
+                if (p.k.NetIsCellInet())
+                    return;
+
+#if NETFX_CORE
+
+                string sTitle = sUrl;
+                sTitle = sTitle.Replace(BaseServiceUri + "/p/store/", "");
+                if (sTitle.ToLower().Contains("myproduction/models/"))
+                {
+                    // http://../p/store/Public/MyProduction/Models/A/Ad/AdrianaLima/Models.001/Adriana Lima Flood166.jpg
+                    // //Public/MyProduction/Models/A/Ad/AdrianaLima/Models.001/Adriana Lima Flood166.jpg
+                    sTitle = sTitle.Substring("Public/MyProduction/Models/".Length);
+                    // A/Ad/AdrianaLima/Models.001/Adriana Lima Flood166.jpg
+                    if (sTitle.Substring(1, 1) == "/")
+                    {
+                        sTitle = "Models//" + sTitle.Substring(5);
+                    }
+                    else
+                    {
+                        sTitle = "Models/" + sTitle;
+                    } // dla Covers, Misc, itp.
+                }
+
+                if (sTitle.ToLower().Contains("myproduction/ero/"))
+                {
+                    sTitle = sTitle.Substring("Public/MyProduction/".Length);
+                    sTitle = "//" + sTitle.Substring(5);
+                }
+
+            // sWhereFullFoto = " ( ( path LIKE '%\FotoVideo\Analogowe\Zdjecia\%' OR path LIKE '%\FotoVideo\Cyfrowe\Zdjecia\%' ) AND path NOT LIKE '%\Ego\%' AND path NOT LIKE '%\Grlfrn\%' AND name LIKE '%jpg' ) "
+            // sWherePublic = " path LIKE '%\Public\%' "
+            // sWhereRodzina = " ( path LIKE '%\FotoVideo\Analogowe\Zdjecia\dMilek\%' OR  path LIKE '%\FotoVideo\Analogowe\Zdjecia\Tata\%' AND name LIKE '%jpg' ) "
+
+
+            Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().Title = sTitle;   // było "FotoRamka - " &..., ale on dopisuje Fotoramka na końcu (po nazwie)
+#endif
+
+        }
+
+        private void AndroPanel(bool bVisible)
+        {
+#if __ANDROID__
+            if(bVisible)
+                uiAndroPanel.Visibility = Visibility.Visible;
+            else
+                uiAndroPanel.Visibility = Visibility.Collapsed;
+
+#endif 
+        }
+        private async System.Threading.Tasks.Task NextPic()
+            {
+                string sUrl = BaseServiceUri + "/p/dysk/gerrandompic.asp?album=" + _sAlbum;
+                sUrl = await p.k.HttpPageAsync(sUrl, "random id", false);
+                if (!sUrl.StartsWith("http"))
+                    return;
+                _sUrl = sUrl;
+                var oBmp = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri(sUrl));
+                this.uiImage.Source = oBmp;
+                SetTitlePicName(sUrl);
+            AndroPanel(false);
+            }
+        
+            private void uiPic_Tapped(object sender, RoutedEventArgs e)
+            {
+                var oResize = this.uiImage.Stretch;
+                switch (oResize)
+                {
+                    case Stretch.Uniform:
+                        {
+                            this.uiImage.Stretch = Stretch.None;
+                            break;
+                        }
+
+                    case Stretch.None:
+                        {
+                            this.uiImage.Stretch = Stretch.Uniform;
+                            break;
+                        }
+                }
+
+            AndroPanel(true);
+
+        }
+
+        private void uiGetInfo_Click(object sender, RoutedEventArgs e)
+            {
+            }
+
+            private void uiGetUrl_Click(object sender, RoutedEventArgs e)
+            {
+            p.k.ClipPut(_sUrl);
+            p.k.DialogBox(_sUrl);
+            }
+
+            private void TykanieStart()
+            {
+                try
+                {
+                    moTimer = new DispatcherTimer();
+                    moTimer.Interval = TimeSpan.FromSeconds(_iTickTime);
+                    moTimer.Tick += Timer_TickUI;
+                    // mTimerMode = 0
+                    moTimer.Start();
+                    return;
+                }
+                catch (Exception ex)
+                {
+                p.k.CrashMessageExit("@InitTykanie", ex.Message);
+                }
+            }
+
+            private void TykanieStop()
+            {
+                if (moTimer is object)
+                {
+                    try
+                    {
+                        moTimer.Tick -= Timer_TickUI;
+                        moTimer.Stop();
+                        moTimer = null;
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                }
+            }
+
+            private async void Timer_TickUI(object sender, object e)
+            {
+                // If mbInDebug Then Debug.WriteLine("BasicCamera:Timer_TickUI, " & Date.Now.ToString("HH:mm:ss"))
+
+                moTimer?.Stop();
+                await NextPic();
+                moTimer?.Start();
+            }
+
+            private void Page_Loaded(object sender, RoutedEventArgs e)
+            {
+                // wczytanie zmiennych z SetSettings i ustawienie ToggleMFI
+                _sAlbum = p.k.GetSettingsString("albumName", "public");
+                _iTickTime = p.k.GetSettingsInt("tickTime", 60);
+                SetCheckMarks();
+            }
+
+            private void SetCheckMarks()
+            {
+                string sDelay = _iTickTime.ToString();
+                bool bSelected = false;
+                foreach (ToggleMenuFlyoutItem oMFI in this.uiMenuDelay.Items)
+                {
+                    if (oMFI.Name.EndsWith(sDelay))
+                    {
+                        oMFI.IsChecked = true;
+                        bSelected = true;
+                    }
+                    else
+                    {
+                        oMFI.IsChecked = false;
+                    }
+                }
+
+
+                if (!bSelected)
+                    this.uiDelay60.IsChecked = true;
+                foreach (ToggleMenuFlyoutItem oMFI in this.uiMenuAlbum.Items)
+                {
+                    if (oMFI.Name.EndsWith(_sAlbum))
+                    {
+                        oMFI.IsChecked = true;
+                        bSelected = true;
+                    }
+                    else
+                    {
+                        oMFI.IsChecked = false;
+                    }
+                }
+
+                if (!bSelected)
+                    this.uiAlbumPublic.IsChecked = true;
+            }
+
+            private void Page_LosingFocus(UIElement sender, LosingFocusEventArgs args)
+            {
+                if (!p.k.NetIsCellInet())
+                    return;
+                moTimer?.Stop();
+            }
+
+            private void Page_GotFocus(object sender, RoutedEventArgs e)
+            {
+                if (!p.k.NetIsCellInet())
+                    return;
+                moTimer?.Start();
+            }
+
+            private void uiGoNext_Click(object sender, RoutedEventArgs e)
+            {
+                Timer_TickUI(null, null);
+            }
+        }
+    }
+
+
